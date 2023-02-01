a952520874f16b2f65be9e7c9bfbda8fae300bfb
piyawat-wir <77828805+piyawat-wir@users.noreply.github.com>
2023-02-01 17:29:35 +0700

    Add Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: {
+		main: "./src/main.ts",
+	},
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	plugins: [
+		new MiniCssExtractPlugin(),
+		new HtmlWebpackPlugin({
+			title: 'A* Star PathFinding Visualization',
+			templateContent: `<canvas></canvas>`,
+			inject: 'body'
+		})
+	],
+	devtool: "source-map",
+	resolve: {
+		extensions: [".ts", ".js"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts?$/,
+				use: "ts-loader",
+				exclude: "/node_modules/",
+				generator: {
+					filename: 'js/[name][ext]'
+				}
+			},
+			{
+				test: /\.js$/,
+				use: "source-map-loader"
+			},
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader"],
+			},
+		],
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'dist'),
+		},
+		port: 3000,
+		hot: true,
+		liveReload: true,
+	},
+};
